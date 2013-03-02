93f573f6dcfb3da3fcff3dcad35f73644eea6fd0
Matt Styles <matt@veryfizzyjelly.com>
2013-03-02 10:21:27 +0000

    Added config file to the public (i.e. custom) directory

---
+++ public/config.js
@@ -0,0 +1,68 @@
+/*
+ * viperfish
+ * https://github.com/mattstyles/viperfish
+ *
+ * Copyright (c) 2013 Matt Styles
+ * Licensed under the MIT license.
+ */
+
+/**
+ * Custom config object
+ * --
+ * These settings will override the default settings
+ * --
+ * To use at the moment simply copy this into the public folder of the project and alter the settings as you need
+ * This will be done automatically by the install process when there is one!
+ * @type {Object}
+ */
+module.exports = {
+    // Module Options
+    moduleOpts: {
+        nav         : [],
+        port        : 3001,
+        customPath  : '/public/',
+        favicon     : 'favicon.png'
+    },
+
+    // Template Options
+    tmplOpts: {
+
+        engine  : 'hogan',
+        path    : './default/'
+
+    },
+
+    // GithubApi Config Options
+    githubapi : {
+
+        apiOpts : {
+            version     : '3.0.0',
+            timeout     : 5000
+        },
+
+        contentOpts : {
+            user        : 'mattstyles',
+            repo        : 'vpf-def',
+            path        : null
+        }
+    },
+
+    // Marked Config Options
+    markedOpts : {
+
+        gfm         : true,
+        tables      : true,
+        breaks      : false,
+        pedantic    : false,
+        sanitize    : false,
+        smartLists  : true,
+        langPrefix  : 'language-',
+        highlight   : function ( code, lang ) {
+            // Javascript highlighting
+            if ( lang === 'js' ) {
+                return highlighter.javascript(code);
+            }
+            return code;
+        }
+    }
+}
